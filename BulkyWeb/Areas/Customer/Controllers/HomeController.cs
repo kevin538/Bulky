57947b49e0b39263c7de51b058b53ed8385f5219
kevin <kevinchanga99@gmail.com>
2024-04-15 18:46:04 -0400

    End of product details added

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 
+using Bulky.DataAccess.Repository;
+using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -8,16 +10,23 @@ namespace BulkyWeb.Areas.Customer.Controllers
     [Area("Customer")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ILogger<HomeController> _logger;   
+        private readonly IUnitOfWork _unitofwork;
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitofwork)
         {
+            _unitofwork = unitofwork;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> ProductList = _unitofwork.Product.GetAll(includeProperties: "Category");
+            return View(ProductList);
+        }
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitofwork.Product.Get(u => u.Id == productId, includeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
